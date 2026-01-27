461abe3994a2e69fa03c1a35eff4ce49f218d948
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2026-01-27 08:33:24 +0530

    client interface
    first commit

---
+++ client/src/data/features.tsx
@@ -0,0 +1,20 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Our AI analyzes video content to suggest the most cluckable concepts.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye-Catching Designs",
+        description: "Generate vibrant, high-contrast thumbnials that stand out in the feed",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Get fully layered design you can tweak to perfection if needed.",
+    },
+
+];
---
+++ client/src/pages/MyGeneration.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyGeneration = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyGeneration
+
