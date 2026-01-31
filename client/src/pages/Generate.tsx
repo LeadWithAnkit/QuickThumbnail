09ffb289c158a7766ef8ba3f625072abb23d5b1e
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2026-01-31 22:19:14 +0530

    .

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,8 +1,47 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom"
+import type { IThumbnail } from "../assets/assets";
+import SoftBackdrop from "../components/SoftBackdrop";
+import { button } from "motion/react-client";
 
 const Generate = () => {
+  const {id}= useParams();
+  const [title,setTitle]= useState('');
+  const [additionalDetails,setadditionalDetails]= useState('');
+  const [thumbnail,setthumbnail]= useState<IThumbnail | null>(null);
+  const [loading, setloading] = useState(false);
+
   return (
     <div>
-      
+       <SoftBackdrop/>
+       <div className="pt-24 min-h-screen">
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8">
+          <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+            {/* LEFT PANNEL*/}
+             <div className={`space-y-6 ${id && 'pointer-events-none'} `}>
+                <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6" >
+                   <div>
+                    <h2 className="text-xl font-bold text-zinc-100 mb-1"> Create Your Thumbnail</h2>
+                    <p className="text-sm text-zinc-400">Describe your vision and let AI bring it to life</p>
+                   </div>
+                   <div className="space-y-5">
+                        
+                   </div>
+                   {/* Button */}
+                   {!id && (
+                    <button className="text-[15px] w-full py-3.5 rounded-xl">
+                      {loading ? 'Generating...': 'Generate Thumbnail'}
+                    </button>
+                   )}
+
+                </div>
+             </div>
+             {/* RIGHT PANNEL */}
+             <div> </div>
+          </div>
+           </main> 
+
+       </div>
     </div>
   )
 }
