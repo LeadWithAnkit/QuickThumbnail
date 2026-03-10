f482037d1950d1a86385816cf8640bae1f5b51d2
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2026-03-10 13:09:10 +0530

    Update ai.ts

--- server/configs/ai.ts
+++ server/configs/ai.ts
@@ -4,15 +4,15 @@ const ai = {
   async generateImage(prompt: string) {
 
     const response = await axios.post(
-      "https://router.huggingface.co/hf-inference/models/SG161222/RealVisXL_V4.0/pipeline/text-to-image",
+      "https://api-inference.huggingface.co/models/SG161222/RealVisXL_V4.0",
       {
         inputs: prompt
       },
       {
         headers: {
           Authorization: `Bearer ${process.env.HF_API_KEY}`,
-          Accept: "image/png",
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          Accept: "image/png"
         },
         responseType: "arraybuffer"
       }
